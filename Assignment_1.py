049dbceba11ea2f6329838cb6e5e70e4995c52f1
patanjal mishra <72647085+patanjalm@users.noreply.github.com>
2021-12-15 12:20:52 +0530

    ineuron_Assignment_1

---
+++ Assignment_1.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+
+# a)   *  = Expression
+# 
+# b)  'hello' = Values
+# 
+# c) -87.8 = values
+# 
+# d)   - = Expression
+# 
+# e)   / = Expression
+# 
+# f)   +	 =Expression
+# 
+# g)   6  =Values
+# 
+
+# 2. What is the difference between string and variable?
+
+# String:- Any character enclose in inverted commas are know as string.eg:-"This is String"
+#         
+# Variable:- Variable is a kind of container which store some value.It can not start with number, it can be start from letter or underscore.Variable have a characterstic of case-sensetive        
+
+# 3. Describe three different data types.
+
+# String Data Types:- If a charater is inclosed in inverted commas than that comes under string data types
+#     
+# Numric Data Types:- It store numerical values.
+#     under this Int,Float,Complex Data Type comes
+#     
+#     int data types :- Int data types represent Whole number.eg:-1
+#     
+#     float data types:-  float data types represent decimal values.eg:-4.5
+#     
+#     complex data types :- It  is the combination of real and imaginary number.4+4j
+#         
+# Boolean data Types:- It store True and False value Or 1 and 0.
+# 
+
+# 
+# 4. What is an expression made up of? What do all expressions do?
+# 
+
+# Expression is made up of constrains, values and operaters.eg:-5*4=20
+# 
+#     Exception are use to evalutes the values and gives certain result
+
+# 5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+# Expression should always have some values ,operater and on performing the expression it always give you some results.
+# 
+# but statement is the python intruction which is executed by python interpreter.
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+# 
+
+# 23
+
+# 7. What should the values of the following two terms be?
+# 
+# 'spam' + 'spamspam'
+# 
+# 'spam' * 3
+# 
+
+# Ans:-'spamspamspam'
+# 
+#     'spamspamspam'
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# because as per the variable charecterstic variable name will not start with numbric value.Thats why egg is the vaild vafriable and 100 is invalid
+
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+# int() 
+# 
+# float()
+# 
+# str()
+# 
+
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten ' + 99 + ' burritos.'
+# 
+
+# This expression cause error because in this expression we are adding string and integer which is not possible.To fix this error we have to perform type casting where we have to convert integer value 99 into string while placing it into inverted commas.
+# 
+# now the correct expression is:- 'I have eaten ' + '99' + ' burritos.'
+
+# In[ ]:
+
+
+
+
